f214003a64c2ba3fa3c5c547337e5d00fbc52034
JamesTheAxolotl <87155032+CostocoPizza@users.noreply.github.com>
2023-12-23 20:28:33 -0500

    footer + format code

--- js/splashTexts.js
+++ js/splashTexts.js
@@ -1,10 +1,15 @@
 var splashTextsList = [
     "Skibidi Toilet",
     "Costco Best",
-    "UwW",
+    "UwU",
     "OwO",
     "100% Skidded",
-    "Best skid on the web"
+    "Best skid on the web",
+    "Int Bg Rity",
+    "Rocoo Taco",
+    "Fuck Mrs Smith me and all my homies hate Mrs Smith",
+    "Do You Have A Snowflake?",
+    "But he made graduation"
 ]
 var randomSplashIndex = Math.floor(Math.random() * splashTextsList.length);
 var randomSplashText = splashTextsList[randomSplashIndex];
