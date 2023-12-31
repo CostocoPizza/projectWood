da28fbaac8c82ca10b58d4488c7141eb3060bcec
JamesTheAxolotl <87155032+CostocoPizza@users.noreply.github.com>
2023-12-31 16:17:02 -0500

    games page p2

--- assets/gameList.js
+++ assets/gameList.js
@@ -1,8 +1,43 @@
-var gameList = [
-    {
-        id: 'exampleid',
-        title: 'Example Name',
-        url: 'Google.com',
-        image: 'Google.com'
-    }
-]
+var gameList = [{}, {
+    id: "exampleID",
+    title: "Example Title",
+    url: "google.com",
+    image: "https://image.freepik.com/free-vector/404-error-abstract-concept-illustration_335657-2243.jpg"
+  },
+  {
+    id: "exampleID",
+    title: "Example Title",
+    url: "google.com",
+    image: "https://image.freepik.com/free-vector/404-error-abstract-concept-illustration_335657-2243.jpg"
+  },
+  {
+    id: "exampleID",
+    title: "Example Title",
+    url: "google.com",
+    image: "https://image.freepik.com/free-vector/404-error-abstract-concept-illustration_335657-2243.jpg"
+  },
+  {
+    id: "exampleID",
+    title: "Example Title",
+    url: "google.com",
+    image: "https://image.freepik.com/free-vector/404-error-abstract-concept-illustration_335657-2243.jpg"
+  },
+  {
+    id: "exampleID",
+    title: "Example Title",
+    url: "google.com",
+    image: "https://image.freepik.com/free-vector/404-error-abstract-concept-illustration_335657-2243.jpg"
+  },
+  {
+    id: "exampleID",
+    title: "Example Title",
+    url: "google.com",
+    image: "https://image.freepik.com/free-vector/404-error-abstract-concept-illustration_335657-2243.jpg"
+  },
+  {
+    id: "exampleID",
+    title: "Example Title",
+    url: "google.com",
+    image: "https://image.freepik.com/free-vector/404-error-abstract-concept-illustration_335657-2243.jpg"
+  }
+];
