ef0adc2b6fb34b139d7dc3ea1add9f2153771b52
JamesTheAxolotl <87155032+CostocoPizza@users.noreply.github.com>
2024-01-09 19:39:43 -0500

    games thing is done + added animated gradients to box on games page

--- assets/gameList.js
+++ assets/gameList.js
@@ -1,4 +1,10 @@
 var gameList = [{}, {
+    id: "slope",
+    title: "Slope",
+    url: "https://66b7d07a-fc66-4076-bda8-9ab6f192554f-00-3gz8iwr9iimje.hacker.replit.dev/",
+    image: "https://1player.games/wp-content/uploads/2020/10/slope.jpg"
+  },
+  {
     id: "exampleID",
     title: "Example Title",
     url: "google.com",
